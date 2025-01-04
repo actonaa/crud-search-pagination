0dd26d70fe21cb137febcc0c2a0609121e87d914
actonaa <actonaputra@gmail.com>
2025-01-05 03:26:23 +0700

    Search and Debounce

--- lib/data.ts
+++ lib/data.ts
@@ -1,8 +1,19 @@
 import { prisma } from "./prisma";
 
-export const getContacts = async () => {
+export const getContacts = async (query: string, currentPage: number) => {
   try {
-    const contacts = await prisma.contact.findMany();
+    const contacts = await prisma.contact.findMany({
+      where: {
+        OR: [
+          {
+            name: { contains: query, mode: "insensitive" },
+          },
+          {
+            phone: { contains: query, mode: "insensitive" },
+          },
+        ],
+      },
+    });
     return contacts;
   } catch (error) {
     console.error("Failed to fetch contacts data", error);
